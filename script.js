d22fb923228ba84eb7c4d41912aba4e5c0fc77b4
anubhav7747 <anubhavtiwari007005@gmail.com>
2023-09-24 13:23:14 +0530

    added email

---
+++ script.js
@@ -0,0 +1,7 @@
+// document.getElementById("menu").style.display = "block";
+// document.getElementById("mobile-menu").style.display = "none";
+
+// if ((navigator.userAgent.match(/iPhone/i)) || navigator.userAgent.match(/Android/i)) {
+//     document.getElementById("menu").style.display = "none";
+//     document.getElementById("mobile-menu").style.display = "block";
+// }
