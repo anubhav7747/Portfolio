7aa4fb3cb15932c8a457413c9ccfb18218e33217
anubhav7747 <anubhavtiwari007005@gmail.com>
2023-09-29 22:47:58 +0530

    updated main, script

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// function myFun(x) {
-//     x.classList.toggle("change");
-
-//     let y = document.getElementById("menu");
-//     // let bodyy = document.getElementsByTagName("body")[0];
-
-//     if (y.style.display === "block") {
-//         y.style.display = "none";
-//     } else {
-//         y.style.display = "block";
-//     }
-// }
-
 function closeNav() {
     document.getElementById("menu").style.width = "0%";
 }
